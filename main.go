cbd1347d45f9808cf7777478d32720d72f930b4e
veitch <wangweiqi@bilibili.com>
2019-11-07 19:59:29 +0800

    fix

--- main.go
+++ main.go
@@ -20,7 +20,10 @@ func main()  {
 		fmt.Fprintf(w, "{\"code\":0,\"message\":\"0\",\"ttl\":1,\"data\":{\"id\":333,\"username\":\"wangweiqi\",\"email\":\"wangweiqi@bilibili.com\",\"token\":\"b87c0de1-dff0-4343-838f-947b06f2ef65\",\"CTime\":\"2019-10-23T19:15:12+08:00\",\"UTime\":\"2019-10-23T19:15:12+08:00\"}}")
 	})
 	http.HandleFunc("/cover/admin/inc/detail", func(w http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(w, "{\"code\":0,\"message\":\"0\",\"ttl\":1,\"data\":{\"name\":\"thum_service\",\"env\":\"uat\",\"cover_total\":0,\"func_cover_total\":1,\"hit_line_total\":2,\"miss_line\":3,\"snapshot_id\":2,\"coverages\":[{\"path\":\"src/app/admin/cmd\",\"cover\":80,\"func_cover\":70,\"branch_cover\":70,\"is_dir\":true,\"hit_line\":2333,\"miss_line\":34}]}}")
+		fmt.Fprintf(w, "{\"code\":0,\"message\":\"0\",\"ttl\":1,\"data\":{\"name\":\"thum_service\",\"env\":\"uat\",\"cover_total\":0,\"func_cover_total\":1,\"hit_line_total\":2,\"miss_line\":3,\"snapshot_id\":2,\"coverages\":[{\"path\":\"src/app/admin/cmd\",\"cover\":80,\"func_cover\":70,\"branch_cover\":70,\"is_dir\":true,\"hit_line\":2333,\"miss_line\":34}, {\"path\":\"src/app/admin/cmd/main.go\",\"cover\":560,\"func_cover\":90,\"branch_cover\":20,\"is_dir\":false,\"hit_line\":2333,\"miss_line\":34}]}}")
+	})
+	http.HandleFunc("/cover/admin/file/detail", func(w http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(w, "{\"code\":0,\"message\":\"0\",\"ttl\":1,\"data\":{\"name\":\"thum_service\",\"env\":\"uat\",\"cover_total\":0,\"func_cover_total\":1,\"hit_line_total\":2,\"miss_line\":3,\"snapshot_id\":2,\"coverages\":[{\"num\": 1,\"hit\": 3,\"content\": \"import (\"},{\"num\": 2,\"hit\": 90,\"content\": \"  fmt\"},{\"num\": 3,\"hit\": 14,\"content\": \"func main(){\"},{\"num\": 4,\"hit\": 43,\"content\": \"flat.prase()\"}]}}")
 	})
 	http.HandleFunc("/cover/admin/add", add)
 	log.Fatal(s.ListenAndServe())
